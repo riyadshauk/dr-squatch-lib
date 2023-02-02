e218c89b3c5312587f176cd9c82d5e1d5695bdc3
Riyad Shauk <riyad@drsquatch.com>
2023-02-02 11:12:18 -0800

    update SQS event types (coming into lambda function via SQS)

--- src/aws/sqs.ts
+++ src/aws/sqs.ts
@@ -1,3 +1,22 @@
 import { SQS } from 'aws-sdk';
 
 export const sqs = new SQS();
+
+export interface SQSQueueEvent {
+  Records: SQSQueueRecord[];
+}
+
+export interface SQSQueueRecord {
+  messageId: string;
+  receiptHandle: string;
+  body: string;
+  attributes: null[];
+  messageAttributes: SQSMessageAttributes;
+  md5OfBody: string;
+  eventSource: string;
+  eventSourceARN: string;
+  awsRegion: string;
+}
+
+export interface SQSMessageAttributes {
+}
