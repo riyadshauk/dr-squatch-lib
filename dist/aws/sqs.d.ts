55871ebf8d4141ecabd2063f69fb4efe212ee43d
Riyad Shauk <riyad@drsquatch.com>
2023-02-06 21:03:54 -0800

    remove SQSQueueEvent as custom type, as aws-lambda already provides it better as SQSEvent

--- dist/aws/sqs.d.ts
+++ dist/aws/sqs.d.ts
@@ -1,18 +1,2 @@
 import { SQS } from 'aws-sdk';
 export declare const sqs: SQS;
-export interface SQSQueueEvent {
-    Records: SQSQueueRecord[];
-}
-export interface SQSQueueRecord {
-    messageId: string;
-    receiptHandle: string;
-    body: string;
-    attributes: null[];
-    messageAttributes: SQSMessageAttributes;
-    md5OfBody: string;
-    eventSource: string;
-    eventSourceARN: string;
-    awsRegion: string;
-}
-export interface SQSMessageAttributes {
-}
